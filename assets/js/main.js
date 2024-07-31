43cb8164e056ae806c4389b34777db6835371daf
jahir <supersimple.jahir@gmail.com>
2024-07-31 13:55:50 +0600

    [Done] blog details page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,88 +1,18 @@
-// nice-select2
-document.addEventListener("DOMContentLoaded", function (e) {
-  var els = document.querySelectorAll(".selectize");
-  els.forEach(function (select) {
-    NiceSelect.bind(select);
-    console.log(select);
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionTitles = document.querySelectorAll(".accordion-title");
+
+  accordionTitles.forEach((title) => {
+    title.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+      const arrow = this.querySelector(".arrow");
+
+      if (content.style.display === "block") {
+        content.style.display = "none";
+        arrow.classList.remove("rotate");
+      } else {
+        content.style.display = "block";
+        arrow.classList.add("rotate");
+      }
+    });
   });
 });
-
-// swiper js
-var swiper1 = new Swiper(".mySwiper1", {
-  slidesPerView: 1,
-  navigation: {
-    nextEl: "#button-next-relacionados",
-    prevEl: "#button-prev-relacionados",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-  },
-});
-
-var swiper2 = new Swiper(".mySwiper2", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-
-  navigation: {
-    nextEl: "#meetExpertNext",
-    prevEl: "#meetExpertPrev",
-  },
-
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-    1400: {
-      slidesPerView: 4,
-      spaceBetween: 50,
-    },
-  },
-});
-
-var swiper3 = new Swiper(".mySwiper3", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-
-  navigation: {
-    nextEl: "#universityNext",
-    prevEl: "#universityPrev",
-  },
-
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-  },
-});
-
-// AOS
-AOS.init();
