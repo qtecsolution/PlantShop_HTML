05bc90cad77c22b9f1165c6d9a6fe1c864a3e388
jahir <supersimple.jahir@gmail.com>
2024-08-01 14:00:58 +0600

    [Done] product review section

--- assets/js/swiper/swiper-custome.js
+++ assets/js/swiper/swiper-custome.js
@@ -6,7 +6,6 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
   breakpoints: {
-
     640: {
       slidesPerView: 1,
       spaceBetween: 10,
@@ -26,5 +25,32 @@ var swiper = new Swiper(".shopPageSlider", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".recentlyDiscountedSlider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
   },
 });
