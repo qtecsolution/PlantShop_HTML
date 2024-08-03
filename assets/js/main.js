0fdc2dab3f31176918f088eb3c32f51729a382d1
jahir <supersimple.jahir@gmail.com>
2024-08-03 14:55:18 +0600

    [Done] some issue fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const accordionTitles = document.querySelectorAll(".accordion-title");
-
-  accordionTitles.forEach((title) => {
-    title.addEventListener("click", function () {
-      const content = this.nextElementSibling;
-      const arrow = this.querySelector(".arrow");
-
-      if (content.style.display === "block") {
-        content.style.display = "none";
-        arrow.classList.remove("rotate");
-      } else {
-        content.style.display = "block";
-        arrow.classList.add("rotate");
-      }
-    });
+document.addEventListener("DOMContentLoaded", function (e) {
+  var els = document.querySelectorAll(".shortBySelect");
+  els.forEach(function (select) {
+    NiceSelect.bind(select);
   });
 });
